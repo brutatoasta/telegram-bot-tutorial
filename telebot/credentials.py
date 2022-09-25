24f93080eead654bbaa8aa1ca8ea1f9da32523aa
brutatoasta <60847718+brutatoasta@users.noreply.github.com>
2022-09-25 16:33:23 -0400

    add credentials

--- telebot/credentials.py
+++ telebot/credentials.py
@@ -1,3 +1,4 @@
-bot_token = "BOT-TOKEN-GOES-HERE"
-bot_user_name = "BOT-USER-NAME"
-URL = "THE-HEROKU-APP-LINK"
+# here goes your access token from BotFather
+bot_token = "5502182608:AAF5w65T47beDEY6A15W367m1H8LbrDIVbw"
+bot_user_name = "RoastmeBot"
+URL = " https://roastem.herokuapp.com/"
